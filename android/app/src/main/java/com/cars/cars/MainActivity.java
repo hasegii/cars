3a9270f8f83d0dd8a11b7eb74cc79e139457cb7c
hasegii <zxc.perm@mail>
2023-10-25 15:51:50 +0500

    init

---
+++ android/app/src/main/java/com/cars/cars/MainActivity.java
@@ -0,0 +1,6 @@
+package com.cars.cars;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/cars/cars/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cars.cars
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
